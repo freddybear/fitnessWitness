6fa73de0b9ea5955b6e24865aeed1be681014071
Fredrik Waltersperger <fredrik.waltersperger@gmail.com>
2017-09-21 21:34:29 +0200

    backend

---
+++ Backend/Backend/Backend.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack;
+using Shared;
+
+namespace Backend.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public  HelloResponse Any(HelloRequest request)
+        {            
+            return new HelloResponse { Result = string.Format("Message recieved from {0}", request.Name) };
+        }
+       
+    }
+}
